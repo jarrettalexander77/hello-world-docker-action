6691bda3be0564930c5d4669cec7caeb13722b2d
jarrettalexander77 <alexander_jarrett@lilly.com>
2020-06-23 14:02:14 -0400

    "update"

--- index.py
+++ index.py
@@ -5,4 +5,4 @@ test_number = sys.argv[2]
 
 print('hello world testing python' + test_name)
 print(f'::set-output name=time::{test_name}')
-print(f'::set-output name=name::{test_number + 1}')
+print(f'::set-output name=name::{str(test_number + 1)}')
