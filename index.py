c9601ae1c5195a72250112e4819be14df0a437d7
jarrettalexander77 <alexander_jarrett@lilly.com>
2020-06-23 14:06:56 -0400

    "adding"

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 import sys
 
 test_name = sys.argv[1]
-test_number = sys.argv[2]
+test_number = str(int(sys.argv[2]) + 10)
 
 print('hello world testing python' + test_name)
 print(f'::set-output name=time::{test_name}')
-print(f'::set-output name=name::{str(test_number + 1)}')
+print(f'::set-output name=name::{test_number}')
