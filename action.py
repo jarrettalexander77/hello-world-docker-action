3441ed7597199da6bc18fd06456308a281dc3a53
jarrettalexander77 <alexander_jarrett@lilly.com>
2020-06-23 14:46:48 -0400

    "update"

--- action.py
+++ action.py
@@ -4,6 +4,7 @@ test_name = sys.argv[1]
 test_number = str(int(sys.argv[2]) + 10)
 
 with open('./README.md', 'r') as f:
+  print('here')
   print(f.read())
 
 print(f'::set-output name=time::{test_name}')
